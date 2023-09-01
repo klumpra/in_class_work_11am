317912daf0441cce750464a631664dcd1a48a94b
klumpra <klumpra@lewisu.edu>
2023-09-01 11:38:01 -0500

    Add ProgramFromCommandLine.java and its class file.

---
+++ ProgramFromCommandLine.java
@@ -0,0 +1,6 @@
+public class ProgramFromCommandLine {
+	public static void main(String[] args) {
+		System.out.println("It is the Friday before a 3-day weekend! Yay!");
+		System.out.println("It is going to be hot. Wear sunscreen.");
+	}
+}
